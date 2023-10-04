3543b49e7af6ceea3f281dbaf78156fcb3f5c2f7
pdiocee <p_diocee@hotmail.com>
2023-10-04 15:16:20 -0600

    switched from app router to pages router

--- app/index.js
+++
@@ -1,9 +0,0 @@
-// import
-
-export default function Home() {
-  return (
-    <main className="">
-      <h1>Welcome to Elmwood Park!</h1>
-    </main>
-  )
-}
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Page() {
+  return (
+    <div>
+      <Head>
+        <title>Elmwood Park Community League</title>
+        <meta name="description" content="This is Elmwood Park Community League's official website." />
+        <link rel="icon" href="favicon.ico" sizes="any"/>
+      </Head>
+      <main className="">
+        <h1>Welcome to Elmwood Park!</h1>
+      </main>
+    </div>
+  )
+}
