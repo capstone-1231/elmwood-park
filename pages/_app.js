1f83b7db35b894df2edff2d691b00f30c2d2f8bd
pdiocee <p_diocee@hotmail.com>
2023-10-04 15:34:33 -0600

    added font support

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,10 @@
+import '@fontsource/libre-baskerville/400.css';
+import '@fontsource/libre-baskerville/700.css';
+import '@fontsource/source-serif-4/400.css';
+import '@fontsource/source-serif-4/700.css';
+import '@fontsource/source-serif-4/900.css';
+import '@fontsource/be-vietnam-pro/400.css';
+
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
   }
