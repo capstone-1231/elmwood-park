8594476b90eea6d0fa07037aa3fd7679f327e3d2
pdiocee <p_diocee@hotmail.com>
2023-10-04 15:15:48 -0600

    added static export method

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+// IMPORTANT - comment out "output: 'export'," whenever you want to start a local server (localhost). if already commented out, leave it alone and go about your business - Pritam
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // output: 'export',
+}
 
 module.exports = nextConfig
