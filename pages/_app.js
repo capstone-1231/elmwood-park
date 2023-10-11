3d39fc9901e09563f1c44f8b2808696d993affc2
pdiocee <p_diocee@hotmail.com>
2023-10-11 14:40:12 -0600

    added theme support

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,11 @@ import '@fontsource/source-serif-4/700.css';
 import '@fontsource/source-serif-4/900.css';
 import '@fontsource/be-vietnam-pro/400.css';
 
+import { ThemeProvider } from '@mui/material';
+import { theme } from '../utils/theme/theme-config.js'
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
   }
