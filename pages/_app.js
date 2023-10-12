a5216a49f05ed36ed7b142f3bf80f03452108d44
pdiocee <p_diocee@hotmail.com>
2023-10-12 15:15:36 -0600

    added a reset stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,13 @@
-import '@fontsource/libre-baskerville/400.css';
-import '@fontsource/libre-baskerville/700.css';
-import '@fontsource/source-serif-4/400.css';
-import '@fontsource/source-serif-4/700.css';
-import '@fontsource/source-serif-4/900.css';
-import '@fontsource/be-vietnam-pro/400.css';
+import '../styles/global.css';
 
 import { ThemeProvider } from '@mui/material';
 import { theme } from '../utils/theme/theme-config.js'
+
+
 
 export default function App({ Component, pageProps }) {
     return <ThemeProvider theme={theme}>
     <Component {...pageProps} />
   </ThemeProvider>
   }
+
