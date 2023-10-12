5c1d060a969a2075f07ba11c9c75021d355b5043
pdiocee <p_diocee@hotmail.com>
2023-10-12 15:16:20 -0600

    displayed top navbar on index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import Navbar from '@/utils/components/header';
+
 import Typography from '@mui/material/Typography';
 
 export default function Page() {
@@ -10,8 +12,11 @@ export default function Page() {
         <meta name="description" content="This is Elmwood Park Community League's official website." />
         <link rel="icon" href="favicon.ico" sizes="any"/>
       </Head>
+      <header>
+      <Navbar />
+      </header>
       <main>
-      <Typography variant="h2" component="h2" color="secondary">Welcome to Elmwood Park</Typography>
+      <Typography variant="h2" component="h2" color="secondary" sx={{ padding: '2rem 1rem' }} >Welcome to Elmwood Park</Typography>
       </main>
     </div>
   )
