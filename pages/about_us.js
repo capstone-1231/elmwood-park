16a3f6e0a0e1e6ef5f82d93477719eccba4ab3d7
pdiocee <p_diocee@hotmail.com>
2023-10-13 18:38:40 -0600

    imported and rendered footer component

--- pages/about_us.js
+++ pages/about_us.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import Navbar from '@/utils/components/header';
+import FooterNav from '@/utils/components/footer';
 
 import Typography from '@mui/material/Typography';
 
@@ -18,6 +19,9 @@ export default function Page() {
       <main>
       <Typography variant="h2" component="h2" color="secondary" sx={{ padding: '2rem 1rem' }} >About Us</Typography>
       </main>
+      <footer>
+        <FooterNav />
+      </footer>
     </div>
   )
 }
