e7a16ddc694913d77a1f6159a06e6637a30d60fd
pdiocee <p_diocee@hotmail.com>
2023-10-21 15:19:27 -0600

    completed responsive hero banner

--- utils/components/banner.js
+++ utils/components/banner.js
@@ -1,54 +1,54 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
-// import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button'
+import Button from '@mui/material/Button';
 import { styled } from '@mui/system';
 
 const ContentContainer = styled('div')({
-    padding: '2rem'
-  });
+  padding: '6rem',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
 const HeroBanner = () => {
   return (
-    <AppBar position="static" sx={{ 
-        width: '100%',
-        background: 'url("river-img.webp") no-repeat',
-        backgroundSize: 'cover',
-        minHeight: '90vh',
-        display: 'flex',
-        alignItems: 'center',
+    <AppBar position="static" sx={{
+      display: 'flex',
+      flexDirection: { xs: 'column', sm: 'column', md: 'row-reverse', lg: 'row-reverse', xl: 'row-reverse' },
     }}>
-      <Box
-        sx={{
-          background: 'rgba(255, 255, 255, 0.9)',
-          padding: '1.2rem',
-          borderRadius: '2rem',
-          width: '50%',
-          textAlign: 'center',
-          position: 'absolute',
-          right: '6rem',
-          top: '12rem'
-        }}
-      >
+      <Box sx={{
+        background: 'url("river-img.webp") no-repeat bottom',
+        backgroundSize: 'cover',
+        width: '100%',
+        minHeight: '40vh',
+      }} />
+      <Box sx={{
+        background: 'rgba(255, 255, 255, 0.9)',
+        padding: '1.2rem',
+        width: '100%',
+        textAlign: 'center',
+        minHeight: '40vh',
+      }}>
         <ContentContainer>
-            <Typography variant='h2' color="text.primary">
-                Elmwood Park Community League
-            </Typography>
-            <Typography variant='body1' color="text.secondary" sx={{ padding: '1rem'}}>
-                Come explore your back yard!
-            </Typography>
-            <Button type="submit" variant="contained" color="primary" disableRipple sx={{ 
+          <Typography variant='h2' color="text.primary">
+            Elmwood Park Community League
+          </Typography>
+          <Typography variant='body1' color="text.secondary" sx={{ padding: '2rem' }}>
+            Come explore your backyard!
+          </Typography>
+          <Button type="submit" variant="contained" color="primary" disableRipple sx={{
             width: '12rem',
             borderRadius: '0.8rem',
             ':hover, :active, :focus': {
-            bgcolor: 'secondary.main',
-            color: '#ffffff',
-            }
-            }}>
+              bgcolor: 'secondary.main',
+              color: '#ffffff',
+            },
+          }}>
             Join us today!
-            </Button>
+          </Button>
         </ContentContainer>
       </Box>
     </AppBar>
@@ -56,3 +56,4 @@ const HeroBanner = () => {
 };
 
 export default HeroBanner;
+
