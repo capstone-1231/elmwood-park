6273313b2370c4e3251a8aae33b4b2d71e276470
pdiocee <p_diocee@hotmail.com>
2023-11-01 12:57:13 -0600

    added date picker wrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
 import '../styles/global.css';
 
 import { ThemeProvider } from '@mui/material';
-import { theme } from '../utils/theme/theme-config.js'
+import { theme } from '../utils/theme/theme-config.js';
 import CssBaseline from '@mui/material/CssBaseline';
 
+import AdapterDayjs from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
+function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Component {...pageProps} />
+      </LocalizationProvider>
+    </ThemeProvider>
+  );
+}
 
-export default function App({ Component, pageProps }) {
-    return <ThemeProvider theme={theme}>
-      <CssBaseline/>
-    <Component {...pageProps} />
-  </ThemeProvider>
-  }
+export default App;
+
 
