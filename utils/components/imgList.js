23a490222ab9a4d5f2901d947c7abfeed4926405
pdhir25 <puneetdhir2000@gmail.com>
2023-11-03 14:18:18 -0600

    carousel component

--- utils/components/imgList.js
+++ utils/components/imgList.js
@@ -1,71 +1,90 @@
 import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import Carousel from 'react-material-ui-carousel';
+import Container from '@mui/material/Container';
 
-export default function imgList() {
+export default function ImgList() {
+  const itemData = [
+    {
+      img: './img/ElmwoodParkBoardRoom.webp',
+      title: 'Board Room',
+    },
+    {
+      img: './img/ElmwoodParkBoardRoom2.webp',
+      title: 'Burger',
+    },
+    {
+      img: './img/ElmwoodParkBoardRoom3.webp',
+      title: 'Camera',
+    },
+  
+    {
+      img: './img/ElmwoodParkDen.webp',
+      title: 'Coffee',
+    },
+    {
+      img: './img/ElmwoodParkDen2.webp',
+      title: 'Hats',
+    },
+    {
+      img: './img/ElmwoodParkDen3.webp',
+      title: 'Honey',
+    }
+  ];
+
   return (
-    <ImageList sx={{ width: 600, height: 400 }} cols={3} rowHeight={164}>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
+    <Container sx={{
+      minHeight: '12vh',
+      display: { xs: "none", sm: "none", md: "none", lg: "block", xl: "block" }
+    }}>
+      <Carousel sx={{ width: '100%' }}>
+        {itemData.map((item) => (
           <img
-            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+            key={item.img}
+            src={item.img}
             alt={item.title}
-            loading="lazy"
+            style={{ width: '100%', height: 'auto' }}
           />
-        </ImageListItem>
-      ))}
-    </ImageList>
+        ))}
+      </Carousel>
+    </Container>
   );
 }
 
-const itemData = [
-  {
-    img: './img/ElmwoodParkBoardRoom.webp',
-    title: 'Board Room',
-  },
-  {
-    img: './img/ElmwoodParkBoardRoom2.webp',
-    title: 'Burger',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-];
+
+
+// {
+//   img: './img/ElmwoodParkDen.webp',
+//   title: 'Coffee',
+// },
+// {
+//   img: './img/ElmwoodParkDen2.webp',
+//   title: 'Hats',
+// },
+// {
+//   img: './img/ElmwoodParkDen3.webp',
+//   title: 'Honey',
+// },
+// {
+//   img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+//   title: 'Basketball',
+// },
+// {
+//   img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+//   title: 'Fern',
+// },
+// {
+//   img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+//   title: 'Mushrooms',
+// },
+// {
+//   img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+//   title: 'Tomato basil',
+// },
+// {
+//   img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+//   title: 'Sea star',
+// },
+// {
+//   img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+//   title: 'Bike',
+// },
---
+++ utils/components/mobileImgCarousel.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import Container from '@mui/material/Container';
+
+export default function MobileImgCarousel() {
+    const itemData = [
+        {
+            img: './img/ElmwoodParkBoardRoom.webp',
+            title: 'Board Room',
+        },
+        {
+            img: './img/ElmwoodParkBoardRoom2.webp',
+            title: 'Board Room',
+        },
+        {
+            img: './img/ElmwoodParkBoardRoom3.webp',
+            title: 'Board Room',
+        },
+        {
+            img: './img/ElmwoodParkDen.webp',
+            title: 'Den',
+        },
+        {
+            img: './img/ElmwoodParkDen2.webp',
+            title: 'Den',
+        },
+        {
+            img: './img/ElmwoodParkDen3.webp',
+            title: 'Den',
+        },
+    ];
+
+    return (
+        <Container sx={{
+            minHeight: '10vh',
+            width: '100%',
+            display: { xs: "block", sm: "block", md: "block", lg: "none", xl: "none" }
+        }}>
+            <Carousel sx={{ width: '100%' }}>
+                {itemData.map((item) => (
+                    <img
+                        key={item.img}
+                        src={item.img}
+                        alt={item.title}
+                        style={{ width: '100%', height: 'auto' }}
+                    />
+                ))}
+            </Carousel>
+        </Container>
+    );
+}
