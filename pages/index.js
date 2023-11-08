a833e1e8319e637160f56c9610fed592c660cc17
pdiocee <p_diocee@hotmail.com>
2023-11-08 14:31:46 -0700

    added components for content areas

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Head from 'next/head';
 import Navbar from '@/utils/components/header';
 import FooterNav from '@/utils/components/footer';
 import HeroBanner from '@/utils/components/banner';
-import ContentWithImageLeft from '@/utils/components/contentAreaImageLeft';
-import ContentWithImageRight from '@/utils/components/contentAreaImageRight';
+import ContentWithImageLeftTruncated from '@/utils/components/contentAreaImageLeftTruncated';
+import ContentWithImageRightTruncated from '@/utils/components/contentAreaImageRightTruncated';
 import HomeCards from '@/utils/components/homeCards';
 import CardCarousel from '@/utils/components/cardCarousel';
 
@@ -24,7 +24,7 @@ export default function Page() {
       </header>
       <main>
       <HeroBanner />
-      <ContentWithImageLeft
+      <ContentWithImageLeftTruncated
         imageSrc="park-image.webp"
         heading="Elmwood Park"
         bodyText="Elmwood Park Community League (EPCL) is a vibrant and active neighborhood organization located in the heart of Edmonton, Alberta, Canada. Founded with a mission to strengthen community bonds, enhance quality of life, and promote a sense of belonging among residents, EPCL has become an integral part of the Elmwood Park neighborhood. With a rich history dating back several decades, the league continues to evolve and adapt to meet the changing needs of its diverse and growing community. One of the primary functions of Elmwood Park Community League is to provide a space where residents can come together, share ideas, and collaborate on initiatives that benefit the entire neighborhood. Regular community meetings and events are organized to facilitate this engagement. These gatherings offer a platform for residents to voice their concerns, propose improvements, and work collectively on projects that enhance the area's livability."
@@ -34,7 +34,7 @@ export default function Page() {
       </Container>
         <HomeCards/>
         <CardCarousel/>
-        <ContentWithImageRight 
+        <ContentWithImageRightTruncated 
           imageSrc="ElmwoodCommunityPark.webp"
           heading="Explore Elmwood Park"
           bodyText="Elmwood Park Community League (EPCL) is a vibrant and active neighborhood organization located in the heart of Edmonton, Alberta, Canada. Founded with a mission to strengthen community bonds, enhance quality of life, and promote a sense of belonging among residents, EPCL has become an integral part of the Elmwood Park neighborhood. With a rich history dating back several decades, the league continues to evolve and adapt to meet the changing needs of its diverse and growing community. One of the primary functions of Elmwood Park Community League is to provide a space where residents can come together, share ideas, and collaborate on initiatives that benefit the entire neighborhood. Regular community meetings and events are organized to facilitate this engagement."
