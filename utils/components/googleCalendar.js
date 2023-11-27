b210648b71d3b475cb6e2bd06835d5b67dd1c8c6
RawthaM <rmurray35@nait.ca>
2023-11-27 13:58:55 -0700

    Event Calendar Adjustments

--- utils/components/googleCalendar.js
+++ utils/components/googleCalendar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Typography } from '@mui/material';
 
 export default function GoogleCalendar() {
     const [iframeHeight, setIframeHeight] = useState('600px');
@@ -24,16 +25,26 @@ export default function GoogleCalendar() {
     }, []);
 
     return (
-        <iframe
-            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23d9e2b5&ctz=America%2FEdmonton&showTitle=0&showTz=0&showCalendars=0&showPrint=0&showTabs=1&showDate=1&showNav=1&src=ZWxtd29vZHBhcmtjbGNhbGVuZGFyQGdtYWlsLmNvbQ&src=ZW4uY2FuYWRpYW4jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%230B8043"
-            style={{
-                display: 'block',
-                margin: '0 auto',
-                width: '90%',
-                height: iframeHeight,
-                border: 'none',
-            }}
-        />
+        <div>
+            <Typography
+                variant="h2"
+                align="center"
+                paddingBottom="1rem"
+            >
+                Event Calendar
+            </Typography>
+            <iframe
+                src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23d9e2b5&ctz=America%2FEdmonton&showTitle=0&showTz=0&showCalendars=0&showPrint=0&showTabs=1&showDate=1&showNav=1&src=ZWxtd29vZHBhcmtjbGNhbGVuZGFyQGdtYWlsLmNvbQ&src=ZW4uY2FuYWRpYW4jaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%230B8043"
+                style={{
+                    display: 'block',
+                    margin: '0 auto',
+                    width: '90%',
+                    height: iframeHeight,
+                    border: 'none',
+                    paddingBottom: '2.5rem',
+                }}
+            />
+        </div>
     );
 }
 
