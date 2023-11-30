a3f883e1ee921395f20eef9d5a249193f5de25fd
pdhir25 <112992718+pdhir25@users.noreply.github.com>
2023-11-29 17:53:58 -0700

    google maps api completed

--- utils/components/googleMaps.js
+++ utils/components/googleMaps.js
@@ -3,31 +3,33 @@ import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/ap
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
-const containerStyle = {
-    width: '100%',
-    height: '500px',
-};
-
-const center = {
-    lat: 53.583973139537335,
-    lng: -113.45711114471045,
-};
-
-const markerPosition = {
-    lat: 53.583973139537335,
-    lng: -113.45711114471045,
-};
-
-const GoogleMaps = () => {
+const GoogleMaps = () => { 
     const [isInfoWindowOpen, setIsInfoWindowOpen] = useState(true)
 
     const handleMarkerClick = () => {
         setIsInfoWindowOpen(!isInfoWindowOpen)
     }  
 
+    const containerStyle = {
+        width: '100%',
+        height: '500px',
+    }
+    
+    const center = {
+        lat: 53.583973139537335,
+        lng: -113.45711114471045,
+    }
+    
+    const markerPosition = {
+        lat: 53.583973139537335,
+        lng: -113.45711114471045,
+    }
+
+    const api = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''
+
     return (
         <Paper elevation={3}>
-            <LoadScript googleMapsApiKey="api-key">
+            <LoadScript googleMapsApiKey={api}>
                 <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
                     {/* Marker component */}
                     <Marker position={markerPosition} onClick={handleMarkerClick} />
