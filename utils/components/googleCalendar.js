587a3e10f448907f4c724223c4851bb7d9b5ebc9
RawthaM <rmurray35@nait.ca>
2023-12-04 14:21:21 -0700

    removed zombie code

--- utils/components/googleCalendar.js
+++ utils/components/googleCalendar.js
@@ -42,7 +42,6 @@ export default function GoogleCalendar() {
                     height: iframeHeight,
                     border: 'none',
                     paddingBottom: '2.5rem',
-                    //.view-cap, .view-container-border
                 }}
             />
         </div>
