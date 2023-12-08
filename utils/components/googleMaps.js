71fb57594636a25198937894ac11c0a2e26a6a31
pdiocee <p_diocee@hotmail.com>
2023-12-08 14:57:54 -0700

    changed key

--- utils/components/googleMaps.js
+++ utils/components/googleMaps.js
@@ -25,7 +25,26 @@ const GoogleMaps = () => {
     lng: -113.45711114471045,
   };
 
-  console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY)
+  const renderInfoWindow = () => (
+    <InfoWindow
+      position={markerPosition}
+      onCloseClick={() => setIsInfoWindowOpen(false)}
+    >
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <img
+          src={"favicon.ico"}
+          alt="Thumbnail"
+          style={{ maxWidth: '100px', maxHeight: '10%', marginRight: '1rem' }}
+        />
+        <div>
+          <Typography variant="subtitle1">Elmwood Park Community League</Typography>
+          <Typography>Community Center and Park</Typography>
+        </div>
+      </div>
+    </InfoWindow>
+  );
+
+  console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY);
 
   return (
     <Paper elevation={3}>
@@ -35,24 +54,7 @@ const GoogleMaps = () => {
           <Marker position={markerPosition} onClick={handleMarkerClick} />
 
           {/* InfoWindow component */}
-          {isInfoWindowOpen && (
-            <InfoWindow
-              position={markerPosition}
-              onCloseClick={() => setIsInfoWindowOpen(false)}
-            >
-              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <img
-                  src={"favicon.ico"}
-                  alt="Thumbnail"
-                  style={{ maxWidth: '100px', maxHeight: '10%', marginRight: '1rem' }}
-                />
-                <div>
-                  <Typography variant="subtitle1">Elmwood Park Community League</Typography>
-                  <Typography>Community Center and Park</Typography>
-                </div>
-              </div>
-            </InfoWindow>
-          )}
+          {isInfoWindowOpen && renderInfoWindow()}
         </GoogleMap>
       </LoadScript>
     </Paper>
