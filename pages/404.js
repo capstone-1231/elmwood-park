4246d53cef2095894ffdd032f96571cd21454e06
pdiocee <p_diocee@hotmail.com>
2023-12-08 13:45:14 -0700

    removed snake game

--- pages/404.js
+++ pages/404.js
@@ -7,9 +7,6 @@ import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
-
-const Snake = dynamic(() => import('snake-game-react'), { ssr: false });
-
 export default function Page() {
   return (
     <div>
@@ -26,19 +23,11 @@ export default function Page() {
           <Typography variant="h1">404</Typography>
           <Typography variant="h5">Page Not Found</Typography>
           <Typography variant="body1">The page you are looking for might be in another Park.</Typography>
-          <Typography variant="body1">Before you leave help Dave the garden snake.</Typography>
           <Link href="/">
             <Button variant="contained" color="primary" size="large" style={{ marginTop: 20 }}>
               Take Me Home!
             </Button>
           </Link>
-          <div style={{padding: '1rem', paddingTop: '3rem'}}>
-            <Snake
-              color1="#50891e"
-              color2="#302400"
-              backgroundColor="#ebebeb"
-            />
-          </div>
         </Container>
       </main>
       <footer>
